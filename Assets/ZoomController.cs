50623b1ed8316b30b16c04afbce898d946726fe2
reysterf <37060445+reysterf@users.noreply.github.com>
2020-05-28 15:34:46 +0800

    Zoom In/Out

---
+++ Assets/ZoomController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomController : MonoBehaviour
+{
+    public Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ZoomIn(){
+        if(camera.orthographicSize >= 3){
+            camera.orthographicSize -= 1;
+        }
+    }
+
+    public void ZoomOut(){
+        if(camera.orthographicSize <= 19){
+            camera.orthographicSize += 1;
+        }
+    }
+}

777d1b0cb813b6ad434fb342b845ca65dbc19e14
reysterf <37060445+reysterf@users.noreply.github.com>
2020-07-13 14:45:43 +0800

    Load and Save UI

---
+++ Assets/DropdownController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropdownController : MonoBehaviour
+{
+    void Start()
+    {
+        List<string> options = new List<string>();
+        options.Add("Woho");
+        options.Add("Wooho");
+        options.Add("Wohoo");
+        //Add Item
+        Dropdown dd = GetComponent<Dropdown>();
+        dd.AddOptions(options);
+    }
+
+}
